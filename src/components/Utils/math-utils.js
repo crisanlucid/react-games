e47b10b092b5e4a2ae315c9a9f0f4b37469db982
crisanlucid <crisan.lucid@yahoo.com>
2019-12-27 16:29:42 +0100

    create Utils Component

---
+++ src/components/Utils/math-utils.js
@@ -0,0 +1,26 @@
+const utils = {
+  // Sum an array
+  sum: arr => arr.reduce((acc, curr) => acc + curr, 0),
+
+  range: (min, max) => Array.from({length: max - min + 1}, (_, i) => min + i),
+
+  random: (min, max) => min + Math.floor(max * Math.random()),
+
+  randomSumIn: (arr, max) => {
+    const sets = [[]];
+    const sums = [];
+    for (let i = 0; i < arr.length; i++) {
+      for (let j = 0, len = sets.length; j < len; j++) {
+        const candidateSet = sets[j].concat(arr[i]);
+        const candidateSum = utils.sum(candidateSet);
+        if (candidateSum <= max) {
+          sets.push(candidateSet);
+          sums.push(candidateSum);
+        }
+      }
+    }
+    return sums[utils.random(0, sums.length)];
+  },
+};
+
+export default utils;
